d38b7ee80da56b40cb619622aa1f41d7b33c8f4a
simondevinci <zhanglinng@outlook.com>
2023-10-23 14:37:40 +1000

    Added folder we will work on

--- IFN557_Week4_Tutorial/Part 1/miltonproject01/create_db.py
+++
@@ -1,6 +0,0 @@
-from miltontours import models, db, create_app
-
-app = create_app()
-ctx = app.app_context()
-ctx.push()
-db.create_all()
--- IFN557_Week4_Tutorial/Part 1/miltonproject01/run.py
+++
@@ -1,6 +0,0 @@
-from miltontours import create_app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
-    
